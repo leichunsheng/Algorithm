281a143b90e4e3eec3fa88b3c3c4fb4184c91a6f
chenzonghui <951798149@qq.com>
2019-06-20 15:21:07 +0800

    希尔排序

--- app/src/main/java/net/lzzy/algorithm/ShellSort.java
+++ app/src/main/java/net/lzzy/algorithm/ShellSort.java
@@ -4,13 +4,31 @@ package net.lzzy.algorithm;
  * Created by lzzy_gxy on 2019/6/15.
  * Description:
  */
-public class ShellSort<T extends Comparable<? super T>>extends BaseSort {
-    ShellSort(T[]items){
+public class ShellSort<T extends Comparable<? super T>> extends BaseSort<T> {
+    ShellSort(T[] items) {
         super(items);
     }
 
     @Override
     public void sort() {
-
+        int len = items.length;
+        int g = len / 2;
+        while (g > 0) {
+            //插入排序，但步长是g
+            for (int i = g; i < items.length; i += g) {
+                int j = i - g;
+                if (bigger(items[i], items[j])) {
+                    continue;
+                }
+                T tmp = (T) items[i];
+                while (j >= 0 && bigger(items[j], tmp)) {
+                    items[j + g] = items[j];
+                    moveStep++;
+                    j -= g;
+                }
+                items[j + g] = tmp;
+            }
+        }
+        g = g / 2;
     }
 }
